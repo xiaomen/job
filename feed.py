1ea373ad91c0bf48ac73b463afa3d9c05e005485
Ruici Luo <luoruici@gmail.com>
2013-05-10 17:14:47 +0800

    bug fix of daemon.

--- feed.py
+++ feed.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from flask import Flask
 from urllib2 import urlopen
 from models import *
+from models.feed import *
+from models.article import *
 from utils import *
 
 logger = logging.getLogger()
@@ -42,48 +44,26 @@ def process_feed(f):
         if not hasattr(entry, 'summary'):
             entry.summary = ''
         logger.info(entry.link)
-        if Article.query.filter_by(link=entry.link).first():
+        if get_article_by(link=entry.link).first():
             break
-        a = Article.create(f.id, \
-                entry.title, \
-                'todo',
-                entry.published, \
-                entry.link, \
-                entry.summary, \
-                entry.author)
+
+        a = add_article(f.id, entry.title, 'todo', entry.published,\
+                entry.link, entry.summary, entry.author)
+
         full_text = decodeHtmlEntity(get_fulltext(a.link))
-        c = ArticleContent(a.id, full_text)
-        db.session.add(c)
+        add_article_content(a.id, full_text)
         logger.info(u'article {0} of feed {1} has been added to db'.\
                 format(a.title, f.name))
 
 def work():
-    feeds = Feed.get_feeds()
+    feeds = get_all_feeds()
     for f in feeds:
         try:
             process_feed(f)
         except:
             logger.error('errors occurs when process feed %s' % f.name)
-    db.session.commit()
 
 def main():
-#    articles = Article.query.all()
-#    for article in articles:
-#        content =ArticleContent.query.filter_by(aid=article.id).first()
-#        if not content:
-#            try:
-#                full_text = decodeHtmlEntity(get_fulltext(article.link))
-#                if not full_text:
-#                    print 'error occurs when getting fulltext of {0}'.format(article.id)
-#                    continue
-#                ArticleContent.create(article.id, full_text)
-#                print(u'article {0} content has been added to db'. \
-#                        format(article.id))
-#            except:
-#                print 'error occurs when getting fulltext of {0}'.format(article.id)
-#
-
-#    work()
     while True:
         work()
         time.sleep(600)
